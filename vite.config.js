b781927c5efe337113cc9017fb55029c0070a168
ducminhtran <50945339+ducminhtran@users.noreply.github.com>
2026-06-01 13:07:14 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/PM/',
   server: { port: 5173, open: true },
   build: { target: 'es2020', sourcemap: true },
   // History-API routing needs index.html served for unknown paths.
